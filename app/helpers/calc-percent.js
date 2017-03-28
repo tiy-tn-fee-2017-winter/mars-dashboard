bbde9b9d939ac85a53045ef0fd840f469b0564ff
John Michael McCasland <jmmccasland@gmail.com>
2017-03-28 18:33:46 -0500

    fixed all the things that CAN be fixed

--- app/helpers/calc-percent.js
+++ app/helpers/calc-percent.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function calcPercent([total, curr]/* , hash*/) {
-  return (curr / total) * 100;
+  return Ember.String.htmlSafe(`width: ${(curr / total) * 100}%`);
 }
 
 export default Ember.Helper.helper(calcPercent);
