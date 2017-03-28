8552cc5d3b14dc1eacc13a02ea68110e1be7c3c9
Evan Hickman <travisevanhickman@gmail.com>
2017-03-28 14:48:13 -0500

    improve dropdown

--- app/ration/new/controller.js
+++ app/ration/new/controller.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  formValues: {
+
+  },
+  types: [
+    'Beverage',
+    'Condiment',
+    'Fresh Food',
+    'Intermediate Moisture',
+    'Irradiated',
+    'Rehydratable Food',
+    'Thermostabilized',
+  ]
 });
