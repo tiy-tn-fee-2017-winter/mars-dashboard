c426aaedfbe6c9aee1f0453cc3ac851d88b9e8dc
justindmurphy33 <justindmurphy33@gmail.com>
2017-03-28 16:26:30 -0500

    added async and await to controller

--- app/crew/new/controller.js
+++ app/crew/new/controller.js
@@ -1,25 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  formValues: {
-    firstName: '',
-    lastName: '',
-    role: '',
-    skills: [],
-    ssn: '',
-    allergies: [],
-    bloodType: '',
-    country: '',
-  },
+  formValues: {},
 
   actions: {
-    submitForm() {
+    async submitForm() {
       const member = this.store.createRecord('crew', this.formValues);
 
-      member.save().then(() => {
-        this.set('formValues', {});
-        this.transitionToRoute('crew.index');
-      });
-    },
+      await member.save();
+
+      this.set('formValues', {});
+      this.transitionToRoute('crew.index');
+    }
   }
 });
